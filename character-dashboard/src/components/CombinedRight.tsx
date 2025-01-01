0ec408b1f19e03c8d88e7ac8bd1df5f30b17797c
argenestel <arpitpali68@gmail.com>
2025-01-02 03:54:10 +0530

    updated the pages

--- character-dashboard/src/components/CombinedRight.tsx
+++ character-dashboard/src/components/CombinedRight.tsx
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import { Card, CardContent } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SwapWidget from "./SwapWidget";
